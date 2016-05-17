fd6440e89aafeca3747022673c7594dd5e265801
roelmann <oelmann.richard@gmail.com>
2016-05-17 18:14:27 +0100

    first commit

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+    $plugin->version = 2013020602;  // YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+	$plugin->release = '2.0'; //plugin release
+	$plugin->maturity   = MATURITY_STABLE;	
+	$plugin->component = 'block_course_contacts'; // Full name of the plugin (used for diagnostics)
+?>
