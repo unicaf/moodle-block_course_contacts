dc465b98427b6423a3616c05be54b59d91c6ba1d
Richard Oelmann <oelmann.richard@gmail.com>
2020-07-28 17:11:28 +0100

    Add MOODLE_INTERNAL call in Lang file

--- lang/en/block_course_contacts.php
+++ lang/en/block_course_contacts.php
@@ -26,6 +26,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die;
+
 $string['alphabetical'] = 'Alphabetical';
 $string['course_contacts'] = 'Course Contacts';
 $string['captcha'] = 'Recaptcha Enabled';
