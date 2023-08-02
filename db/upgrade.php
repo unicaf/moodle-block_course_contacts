0a8f6e50831571658985a629888d508f7c436a47
George Michael <george.michael@unicaf.org>
2023-08-02 12:54:32 +0300

    Update with base64 decode and make changes and then base64 encript

--- db/upgrade.php
+++ db/upgrade.php
@@ -8,12 +8,42 @@ function xmldb_block_course_contacts_upgrade($oldversion): bool {
 
     $dbman = $DB->get_manager();
 
-    if($oldversion < 2020050111){
-       $sql = "UPDATE {block_instances}
-                 SET configdata = :configdata
-                WHERE blockname = :blockname";
-       $DB->execute($sql,['configdata' => 'Tzo4OiJzdGRDbGFzcyI6MTY6e3M6NToiZW1haWwiO3M6MToiMCI7czo3OiJtZXNzYWdlIjtzOjE6IjEiO3M6NToicGhvbmUiO3M6MToiMCI7czoxMToiZGVzY3JpcHRpb24iO3M6MToiMCI7czoxNjoiaGlkZV9ibG9ja19ndWVzdCI7czoxOiIxIjtzOjExOiJlbWFpbF9ndWVzdCI7czoxOiIwIjtzOjEzOiJtZXNzYWdlX2d1ZXN0IjtzOjE6IjAiO3M6MTE6InBob25lX2d1ZXN0IjtzOjE6IjAiO3M6MTc6ImRlc2NyaXB0aW9uX2d1ZXN0IjtzOjE6IjAiO3M6Njoic29ydGJ5IjtzOjE6IjAiO3M6NzoiaW5oZXJpdCI7czoxOiIwIjtzOjExOiJ1c2VfYWx0bmFtZSI7czoxOiIwIjtzOjY6InJvbGVfMSI7czoxOiIwIjtzOjY6InJvbGVfMyI7czoxOiIwIjtzOjY6InJvbGVfNCI7czoxOiIwIjtzOjY6InJvbGVfNSI7czoxOiIwIjt9','blockname' => 'course_contacts']);
-    }
+    if($oldversion < 2020050112){
+
+        $table = "block_instances";
+        $records = $DB->get_records($table,['blockname'=>"course_contacts"],"","id,configdata");
+        $records = json_decode(json_encode($records),true);
+        $keys = array_keys($records);
+        for($i = 0; $i < count($records); $i++){
+                foreach ($records[$keys[$i]] as  $record => $value) {
+
+                    if ($record === "configdata"  && (!empty($value))){
+
+
+                        $decode = base64_decode($value);
+
+                        $decode = unserialize($decode);
+
+                        $decode->email = "0";
+                        $decode->message = "1";
+
+                        $serialize = serialize($decode);
+
+                        $encode = base64_encode($serialize);
+
+                        $value = $encode;
+
+                        $records[$keys[$i]][$record] = $value;
+                    }
+                    $DB->update_record($table,$records[$keys[$i]],true);
+                }
+
+            }
+
+
+
+        }
+
 
     return true;
 }
